39cf0ca803aa04ad50b8be763ce9f170de4c18f9
zanbarovier <51932998+zanbarovier@users.noreply.github.com>
2020-04-02 18:42:40 -0400

    Update data_collection_ECI.m

--- data_collection_ECI.m
+++ data_collection_ECI.m
@@ -8,7 +8,7 @@ root = app.Personality2;
 days=1;
 timestep=1; %Timestep of data acquisition in seconds
 start_time='21 Jun 2022 16:00:00.000';
-stop_time='22 Jun 2022 16:00:00.000';
+stop_time='23 Jun 2022 16:00:00.000';
 scenario = root.Children.New('eScenario','Data_Collection');
 scenario.SetTimePeriod(start_time,stop_time);
 scenario.StartTime = start_time;
@@ -59,15 +59,7 @@ root.ExecuteCommand('SetAttitude */Satellite/Neudose Profile InertFix Euler 0 0 
 root.UnitPreferences.Item('DateFormat').SetCurrentUnit('EpSec');
 
 %--------------------------------------------------------------------------------
-% Save names
-% Bname_eci='eciB_21Jun2022_5Jul2022.mat';
-% BDotname_eci='eciBDot_21Jun2022_5Jul2022.mat';
-% eciname='eci_21Jun2022_5Jul2022.mat';
-% att_eciname='att_eci_21Jun2022_5Jul2022.mat';
-% body_magname='eci_mag_1s_21Jun2022_22Jun2022.mat';
-% vel_eciname='veleci_10s_21Jun2022_5Jul2022.mat';
-% 
-% 
+
 % % ECI Magnetic Field in nT
 mag_elems_eci = {'B Field ECI-X';'B Field ECI-Y'; 'B Field ECI-Z'}; 
 satDP_ecI=satellite.DataProviders.GetDataPrvTimeVarFromPath('SEET Magnetic Field').Exec(scenario.StartTime,scenario.StopTime,timestep); 
@@ -77,40 +69,23 @@ bz_eci = cell2mat(satDP_ecI.DataSets.Item(cast(18,'int32')).GetValues);
 magdata_eci = [bx_eci by_eci bz_eci]*1e-9; %convert to nT
 bdot_eci=diff(magdata_eci)/timestep;
 bdot_eci=[bdot_eci; bdot_eci(end,:)/timestep];
-%save(Bname_eci,'magdata_eci');
-%save(BDotname_eci,'bdot_eci');
 
-% ------------------------------------------------------------------------------------
-% Other data sets
-
-% 
-root.UnitPreferences.Item('DateFormat').SetCurrentUnit('EpSec'); 
-attDP_eci = satellite.DataProviders.Item( 'Attitude Quaternions').Exec(scenario.StartTime,scenario.StopTime,timestep); 
-time =  cell2mat(attDP_eci.DataSets.GetDataSetByName('Time').GetValues); 
-% q1_eci = cell2mat(attDP_eci.DataSets.GetDataSetByName('q1').GetValues); 
-% q2_eci = cell2mat(attDP_eci.DataSets.GetDataSetByName('q2').GetValues); 
-% q3_eci = cell2mat(attDP_eci.DataSets.GetDataSetByName('q3').GetValues); 
-% q4_eci = cell2mat(attDP_eci.DataSets.GetDataSetByName('q4').GetValues); 
-% 
-% 
-%  att_eci=[q4_eci q1_eci q2_eci q3_eci ]; %convert to scalar first for use in MATLAB
-%save(att_eciname,'att_eci');
-% 
-% 
 
 root.UnitPreferences.Item('DateFormat').SetCurrentUnit('EpSec'); 
-inertial = satellite.DataProviders.Item('Vectors(Inertial)').Group.Item('Earth').Exec(scenario.StartTime,scenario.StopTime,timestep);
-x = cell2mat(inertial.DataSets.GetDataSetByName('x').GetValues); 
-y = cell2mat(inertial.DataSets.GetDataSetByName('y').GetValues); 
-z = cell2mat(inertial.DataSets.GetDataSetByName('z').GetValues); 
-vx = cell2mat(inertial.DataSets.GetDataSetByName('Derivative x').GetValues); 
-vy = cell2mat(inertial.DataSets.GetDataSetByName('Derivative y').GetValues); 
-vz = cell2mat(inertial.DataSets.GetDataSetByName('Derivative z').GetValues);
+pos = satellite.DataProviders.Item('Vectors(Body)').Group.Item('Position').Exec(scenario.StartTime,scenario.StopTime,timestep);
+vel = satellite.DataProviders.Item('Vectors(Body)').Group.Item('Velocity').Exec(scenario.StartTime,scenario.StopTime,timestep);
+time = cell2mat(pos.DataSets.GetDataSetByName('Time').GetValues); 
+x = cell2mat(pos.DataSets.GetDataSetByName('x').GetValues); 
+y = cell2mat(pos.DataSets.GetDataSetByName('y').GetValues); 
+z = cell2mat(pos.DataSets.GetDataSetByName('z').GetValues); 
+vx = cell2mat(vel.DataSets.GetDataSetByName('x').GetValues); 
+vy = cell2mat(vel.DataSets.GetDataSetByName('y').GetValues); 
+vz = cell2mat(vel.DataSets.GetDataSetByName('z').GetValues);
 
 pos_eci=[x y z]*1e3; %convert to m from km
 vel_eci=[vx vy vz]*1e3; %covnert to m/s from km/s
-%save(vel_eciname,'vel_eci');
-%  
+
+
 
 % sun stuff
 root.UnitPreferences.Item('DateFormat').SetCurrentUnit('EpSec'); 
