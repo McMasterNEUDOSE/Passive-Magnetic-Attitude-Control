3be4db83cf7ee7fda82be5d3d95768672f47a792
Alexander Barovier <51932998+zanbarovier@users.noreply.github.com>
2020-09-28 11:45:22 -0400

    Add files via upload

---
+++ calcNad.m
@@ -0,0 +1,10 @@
+function Nad = calcNad(t, q, Earth_vector)
+% Calculate angle between Z axis and nadir 
+z=[0, 0, 1];                    % Long axis 
+Nad = zeros(1, length(t));
+
+for  i = 1:length(t)
+    unit_evec         = quatrotate(q(i,:),Earth_vector(i,:)./norm(Earth_vector(i,:)));
+    Nad(:,i)   = atan2d(norm(cross(unit_evec,z)),dot(unit_evec,z));
+end
+end
