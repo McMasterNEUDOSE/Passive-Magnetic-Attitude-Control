5c24bd36396cabb961fdaec9338ef7c1347b604d
Alexander Barovier <51932998+zanbarovier@users.noreply.github.com>
2020-11-07 18:59:45 -0500

    Update calcNad.m

--- calcNad.m
+++ calcNad.m
@@ -5,7 +5,6 @@ Nad = zeros(1, length(t));
 
 for  i = 1:length(t)
     unit_evec         = quatrotate(q(i,:),Earth_vector(i,:)./norm(Earth_vector(i,:)));
-    ang =acosd(dot(unit_evec,z));
-    Nad(:,i)   = min(ang,180-ang);
+    Nad(:,i) = abs(atand(norm(cross(unit_evec,z))/dot(unit_evec,z)));
 end
 end
