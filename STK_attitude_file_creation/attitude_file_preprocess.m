1bafc5f27858b031f61ebcedff824d33b9808e07
Alexander Barovier <51932998+zanbarovier@users.noreply.github.com>
2020-11-25 12:34:54 -0500

    Update attitude_file_preprocess.m

--- STK_attitude_file_creation/attitude_file_preprocess.m
+++ STK_attitude_file_creation/attitude_file_preprocess.m
@@ -5,12 +5,10 @@ tstep = 60; % desired timestep for attitude file
 
 
 q_fname=strcat('q_save_',num2str(days),'days.mat'); % File name of quaternions save 
-w_fname=strcat('w_save_',num2str(days),'days.mat'); % File name of angular velocity save
 t_fname=strcat('t_save_',num2str(days),'days.mat'); % Filename of time save
 
 
 q_downsamp=cell2mat(struct2cell(load(q_fname))); % Import quaternions
-w_downsamp=(180/pi)*cell2mat(struct2cell(load(w_fname))); % Import angular velocities, comvert to deg/s
 t_downsamp=cell2mat(struct2cell(load(t_fname))); % Import time
 
 t_end = t_downsamp(end); %Ending time, final value in downsampled time array
@@ -18,35 +16,30 @@ t_STK = 0:tstep:t_end; % Time vector for STK
 L = round(t_end/tstep); % number of points
 
 q_interp = zeros(L,4); % Initialize interpolated quaternions
-w_interp = zeros(L,3); % Initialize interpolated angular velocites
 for i=1:L
     t_curr = t_STK(i);
     q_interp(i,:)= lininterp1(t_downsamp,q_downsamp,t_curr); % Interpolate
-    w_interp(i,:)= lininterp1(t_downsamp,w_downsamp,t_curr);
 end
 
 % -------------------------------------
 % TESTING STUFF
-%  t_STK = 0:tstep:1000;
+%  t_STK = 0:1:1000;
 %  q_interp = zeros(1000,4);
-%  w_interp = zeros(1000,3);
-% 
 % for i=1:1001
 %     t_curr = t_STK(i);
 %     q_interp(i,:)= lininterp1(t_downsamp,q_downsamp,t_curr);
-%     w_interp(i,:)= lininterp1(t_downsamp,w_downsamp,t_curr);
 % end
 % -------------------------------------
 
 q_STK = [q_interp(:,2:4) q_interp(:,1)]; % Convert from Scalar firt to scalar last
-w_STK = w_interp;
 
 % Saving attitude file
 a_fname = strcat('attitude_',num2str(days),'days_',num2str(tstep),'s.txt'); % Set file name of attitude save file
 
-a_save_STK = [t_STK' q_STK w_STK]; % Maxe combined matrix of time, quaternions, angular velocities
-writematrix(a_save_STK,a_fname,'delimiter','space'); % Save matric to space delimted text file
+a_save_STK = [t_STK' q_STK]; % Maxe combined matrix of time an dquaternions
+   
 
+writematrix(a_save_STK,a_fname,'delimiter','tab'); % Save matrix to space delimted text file
 
 toc;
 
