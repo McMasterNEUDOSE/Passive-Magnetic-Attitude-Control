eaffd9ec99fb9830a096fbf8b12ce71199a6242a
Alexander Barovier <51932998+zanbarovier@users.noreply.github.com>
2021-02-07 20:00:40 -0500

    Delete calcBeta.m

--- calcBeta.m
+++
@@ -1,9 +0,0 @@
-function Beta = calcBeta(t, q, B)
-y=[0, 1, 0];                    % CHANGE THIS to reflect alignment axis 
-Beta = zeros(1, length(t));
-
-for  i = 1:length(t)
-    mag         = quatrotate(q(i,:),B(i,:)./norm(B(i,:)));
-    Beta(:,i)   = atan2d(norm(cross(mag,y)),dot(mag,y));
-end
-end
